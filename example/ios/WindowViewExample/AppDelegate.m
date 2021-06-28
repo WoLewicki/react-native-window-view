c872713dddc3b31e1acfb95b783d509586db2717
Ahmed Eid <a.eid@yandex.com>
2021-06-29 01:57:13 +0200

    chore: add missing snippet to the example app.

--- example/ios/WindowViewExample/AppDelegate.m
+++ example/ios/WindowViewExample/AppDelegate.m
@@ -37,6 +37,15 @@ static void InitializeFlipper(UIApplication *application) {
 
 @implementation RNWindow
 
+- (UIView *) hitTest:(CGPoint)point withEvent:(UIEvent *)event
+{
+    UIView *hitTestResult = [super hitTest:point withEvent:event];
+    if ([hitTestResult isKindOfClass:[RNWindow class]]) {
+      return nil;
+    }
+    return hitTestResult;
+}
+
 - (void)didAddSubview:(UIView *)subview
 {
   if (![subview isKindOfClass:[RNViewContainer class]]) {
